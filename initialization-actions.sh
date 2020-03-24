28da27fc85ee69d6863cda3dc5bbec64b29895ab
Chris Olen <chrisolen1@gmail.com>
2020-03-23 22:25:10 -0500

    various changes

--- initialization-actions.sh
+++ initialization-actions.sh
@@ -2,6 +2,8 @@ pip install wrapt --upgrade --ignore-installed
 
 git clone https://github.com/chrisolen1/sarcasm-detection.git
 
+git config --global user.name "chrisolen1"
+
 gsutil cp gs://sarc-bucket-5/start-jupyter.sh /start-jupyter.sh
 
 sudo chown -R chrisolen:chrisolen /sarcasm-detection
--- start-cluster.sh
+++ start-cluster.sh
@@ -1,6 +1,6 @@
 gcloud dataproc clusters create sarc-cluster --region $REGION --zone $ZONE \
---master-machine-type $MASTER_MCHN_TYPE --num-masters 1 \
---worker-machine-type $WORKER_MCHN_TYPE --num-workers 2 \
+--master-machine-type $MASTER_MCHN_TYPE --num-masters $NUM_MASTERS --master-boot-disk-size $MASTER_SD_SIZE \
+--worker-machine-type $WORKER_MCHN_TYPE --num-workers $NUM_WORKERS --worker-boot-disk-size $WORKER_SD_SIZE \
 --image-version 1.4 --optional-components ANACONDA \
 --metadata 'PIP_PACKAGES=pandas==0.23.0 scipy==1.1.0 tensorflow keras matplotlib sentencepiece tensorflow_hub bert-for-tf2' \
 --initialization-actions gs://$BUCKET_NAME/initialization-actions.sh,gs://goog-dataproc-initialization-actions-${REGION}/python/pip-install.sh
