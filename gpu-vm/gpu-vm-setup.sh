306941abdfa4c54c380da2d58ee659172bd74e4b
chrisolen1 <chrisolen1@gmail.com>
2020-04-25 15:44:32 -0500

    fixes for  gpu-vm

--- gpu-vm/gpu-vm-setup.sh
+++ gpu-vm/gpu-vm-setup.sh
@@ -1,31 +1,6 @@
 # Prophylactically update all the packages, optional.
 sudo apt -y update && sudo apt -y upgrade
 
-sudo apt-get install -yq python3 python3-pip libsndfile1-dev
-pip3 install wrapt --upgrade --ignore-installed
-pip3 install pandas
-pip3 install scipy 
-pip3 install tensorflow-gpu
-pip3 install keras
-pip3 install tensorflow_hub
-pip3 install pomegranate==0.10
-pip3 install cupy
-pip3 install gcsfs
-pip3 install jupyter
-pip3 install librosa
-pip3 install soundfile
-pip3 install python_speech_features
-pip3 install pydub
-pip3 install --upgrade google-cloud-storage
-pip3 install wave
-pip3 install hmmlearn
-
-# cloning repo of interest
-git clone https://github.com/venkunikku/crew-2.git
-
-# jupyter starting script
-gsutil cp gs://ad-bucket-15730/start-jupyter.sh /start-jupyter.sh
-
 ### Set up for Cuda 10.1, cuDNN 7.6.5 ###
 
 # Add NVIDIA package repositories
@@ -38,22 +13,65 @@ sudo apt install -y ./nvidia-machine-learning-repo-ubuntu1804_1.0.0-1_amd64.deb
 sudo apt-get update
 
 # Install NVIDIA driver
-sudo apt-get install -y --no-install-recommends nvidia-driver-430
+sudo apt-get install -y --no-install-recommends nvidia-driver-440
+
 # Doesn't hurt to reboot here and check to see if GPUs are visible via `nvidia-smi`
 
 # Install development and runtime libraries (~4GB)
-sudo apt-get install -y --no-install-recommends \
+sudo apt-get update && \
+    sudo apt-get install -y --no-install-recommends \
     cuda-10-1 \
     libcudnn7=7.6.4.38-1+cuda10.1  \
     libcudnn7-dev=7.6.4.38-1+cuda10.1
 
 
 # Install TensorRT. Requires that libcudnn7 is installed above.
-sudo apt-get install -y --no-install-recommends libnvinfer6=6.0.1-1+cuda10.1 \
+sudo apt-get update && \
+    sudo apt-get install -y --no-install-recommends libnvinfer6=6.0.1-1+cuda10.1 \
     libnvinfer-dev=6.0.1-1+cuda10.1 \
-    libnvinfer-plugin6=6.0.1-1+cuda10.1                                       
+    libnvinfer-plugin6=6.0.1-1+cuda10.1
 
-# changing ownership for write access to repo
+sudo apt-get update
+# purge python2.7
+sudo apt-get autoremove -y --purge python2.7
+# get pip for python3
+sudo apt-get install -yq python3-pip libsndfile1-dev
+# upgrade pip - important for accessing packages e.g. tf2.1.0
+pip3 install --upgrade pip
+pip3 install wrapt --upgrade --ignore-installed
+pip3 install pandas
+pip3 install scipy 
+pip3 install tensorflow-gpu==2.1.0
+pip3 install keras
+pip3 install tensorflow_hub
+pip3 install pomegranate==0.10
+pip3 install gcsfs
+pip3 install jupyter
+pip3 install librosa
+pip3 install soundfile
+pip3 install python_speech_features
+pip3 install pydub
+pip3 install --upgrade google-cloud-storage
+pip3 install wave
+pip3 install hmmlearn
+pip3 install matplotlib
+pip3 install tqdm
+#cupy setup
+pip3 install cupy --no-cache-dir -vvvv
+pip3 install cython
+# sompy
+pip3 install sompy
+wget https://github.com/sevamoo/SOMPY/archive/master.zip && pip3 install master.zip
+pip3 install ipdb
+pip3 install ipdb==0.8.1
+
+# cloning repo of interest
+git clone https://github.com/venkunikku/crew-2.git
+
+# jupyter starting script
+gsutil cp gs://ad-bucket-15730/start-jupyter.sh /start-jupyter.sh
+
+# Changing ownership for write access to repo
 sudo chown -R chrisolen:chrisolen /crew-2
 
 # initializing gh account
